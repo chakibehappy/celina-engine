50e8ca33aec7f4f478d8fcfbb0693f22dbd193e9
chakibehappy <debri.pramiarta@gmail.com>
2026-04-24 00:27:21 +0700

    add test login api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\MobileTestController;
 
+// This is the clean, no-middleware route for testing
+Route::post('/test-login', [MobileTestController::class, 'login']);
+
+// Keep this one if you want to test "Real" auth later
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
