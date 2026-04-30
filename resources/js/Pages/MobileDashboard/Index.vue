ee8a799363823be5b5c4814b1619a11b58f02c20
chakibehappy <debri.pramiarta@gmail.com>
2026-04-30 16:36:38 +0700

    WIP database & table creation on go

--- resources/js/Pages/MobileDashboard/Index.vue
+++ resources/js/Pages/MobileDashboard/Index.vue
@@ -210,12 +210,6 @@
                 </select>
             </div>
             
-            <button v-if="selectedAppId && !activeTable" @click="showSynthModal = true" class="flex items-center gap-2 bg-gradient-to-r from-purple-600 to-blue-600 px-4 py-2 rounded-lg text-xs font-bold hover:scale-105 transition">
-                <span class="material-symbols-outlined text-sm">add_box</span>
-                SYNTHESIZE TABLE
-            </button>
-
-            
             <div v-else class="flex items-center gap-3">
                 <button @click="activeTable = null" class="p-2 hover:bg-gray-700 rounded-full transition text-gray-400">
                     <span class="material-symbols-outlined">arrow_back</span>
@@ -388,7 +382,6 @@
 import { ref, computed, defineComponent, h } from 'vue';
 import { router } from '@inertiajs/vue3';
 import axios from 'axios';
-
 const props = defineProps({ 
     navigations: Array, screens: Array, apps: Array, users: Array, 
     roles: Array, menus: Array, subModules: Array, system_icons: Array, schemas: Object
