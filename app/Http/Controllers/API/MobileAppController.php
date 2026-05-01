acb57be80f7a5fde7795f9615e1b637713bafa4d
chakibehappy <debri.pramiarta@gmail.com>
2026-05-01 17:00:16 +0700

    connect read data to the json format

--- app/Http/Controllers/API/MobileAppController.php
+++ app/Http/Controllers/API/MobileAppController.php
@@ -340,7 +340,7 @@ class MobileAppController extends Controller
                 ->where('id', $id)
                 ->update($data);
 
-            return response()->json(['success' => true, 'affected' => $data]);
+            return response()->json(['success' => true, 'affected' => $request]);
         } catch (\Exception $e) {
             return response()->json(['success' => false, 'message' => $e->getMessage()], 500);
         }
