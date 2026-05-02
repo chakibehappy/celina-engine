0049cfda1defbe9dcdcbf9003e313898451f8a64
chakibehappy <debri.pramiarta@gmail.com>
2026-05-03 00:05:38 +0700

    add filter and pagination on table synth lab

--- resources/js/Pages/MobileDashboard/Index.vue
+++ resources/js/Pages/MobileDashboard/Index.vue
@@ -243,6 +243,7 @@
                     INSERT DATA
                 </button>
             </div>
+            <div class="flex w-full"></div>
              <div v-if="activeTable" class="relative">
                 <span class="material-symbols-outlined absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 text-sm">search</span>
                 <input 
