3b4120763b53264e6d57938577f0231876499439
chakibehappy <debri.pramiarta@gmail.com>
2026-05-05 09:48:41 +0700

    remove unused tables and add new columns on apps table

--- app/Models/App/App.php
+++ app/Models/App/App.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class App extends Model
 {
-    protected $fillable = ['name', 'slug', 'database_name'];
+    protected $fillable = ['name', 'slug', 'database_name', 'starting_screen', 'login_type', 'navigation_type'];
 
     public function users() { return $this->hasMany(AppUser::class); }
     public function screens() { return $this->hasMany(AppScreen::class); }
